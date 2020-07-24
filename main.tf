da4686fbcf0aa771f902268d18eaadf93a90c5a9
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-24 11:24:08 -0300

    BBL-209 improving parameters, make format and terratest temporally deactivated

--- main.tf
+++ main.tf
@@ -9,15 +9,15 @@ data "aws_sns_topic" "main" {
 # CloudWatch Event
 #
 resource "aws_cloudwatch_metric_alarm" "alarm_cw_natgw_active_cx" {
-  count     = var.send_sns ? 1 : 0
+  count = var.send_sns ? 1 : 0
 
   alarm_name          = var.alarm_suffix == "" ? "vpc-natgw-active-cx-count-alarm" : "vpc-natgw-active-cx-count-alarm-${var.alarm_suffix}"
   comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.alarm_evaluation_periods
   datapoints_to_alarm = "1"
   metric_name         = "ActiveConnectionCount"
   namespace           = "AWS/NATGateway"
-  period              = "3600" // 60 mins
+  period              = var.alarm_period
   statistic           = "Maximum"
   threshold           = "0"
   alarm_description   = "VPC Nat Gateway CW ActiveConnectionCount Rule has been triggered"
--- variables.tf
+++ variables.tf
@@ -6,11 +6,23 @@ variable "sns_topic_name" {
 variable "send_sns" {
   type        = bool
   default     = false
-  description = "If true will send message *Successful AWS console login with the root account* to SNS topic"
+  description = "If true will send message *Successful AWS console login with the root account* to SNS topic."
 }
 
 variable "alarm_suffix" {
   type        = string
   default     = ""
   description = "Suffix to add to alarm name, used for separating different AWS account."
+}
+
+variable "alarm_period" {
+  type        = number
+  default     = 3600
+  description = "The period in seconds over which the specified statistic is applied, eg 3600 sec = 60 mins."
+}
+
+variable "alarm_evaluation_periods" {
+  type        = number
+  default     = 1
+  description = "The number of periods over which data is compared to the specified threshold."
 }
