8116e8a7f4ec49be49ff506a20fcfec0773cc6af
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-24 14:34:30 -0300

    BBL-209 adding insufficient_data_actions to notify alarm implying that insufficient data reflects not even a 1st metric event was detected for VpcNatGwActiveCx

--- main.tf
+++ main.tf
@@ -11,15 +11,16 @@ data "aws_sns_topic" "main" {
 resource "aws_cloudwatch_metric_alarm" "alarm_cw_natgw_active_cx" {
   count = var.send_sns ? 1 : 0
 
-  alarm_name          = var.alarm_suffix == "" ? "vpc-natgw-active-cx-count-alarm" : "vpc-natgw-active-cx-count-alarm-${var.alarm_suffix}"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = var.alarm_evaluation_periods
-  datapoints_to_alarm = "1"
-  metric_name         = "ActiveConnectionCount"
-  namespace           = "AWS/NATGateway"
-  period              = var.alarm_period
-  statistic           = "Maximum"
-  threshold           = "0"
-  alarm_description   = "VPC Nat Gateway CW ActiveConnectionCount Rule has been triggered"
-  alarm_actions       = [data.aws_sns_topic.main.arn]
+  alarm_name                = var.alarm_suffix == "" ? "vpc-natgw-active-cx-count-alarm" : "vpc-natgw-active-cx-count-alarm-${var.alarm_suffix}"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = var.alarm_evaluation_periods
+  datapoints_to_alarm       = "1"
+  metric_name               = "ActiveConnectionCount"
+  namespace                 = "AWS/NATGateway"
+  period                    = var.alarm_period
+  statistic                 = "Maximum"
+  threshold                 = "0"
+  alarm_description         = "VPC Nat Gateway CW ActiveConnectionCount Rule has been triggered"
+  alarm_actions             = [data.aws_sns_topic.main.arn]
+  insufficient_data_actions = [data.aws_sns_topic.main.arn]
 }
