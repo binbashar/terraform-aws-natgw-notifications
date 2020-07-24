fadf187f1d32c0f4e729930dabedf2124d72c013
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-24 11:05:47 -0300

    BBL-209 module tested - v0.0.2 ready

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_cloudwatch_metric_alarm" "alarm_cw_natgw_active_cx" {
   count     = var.send_sns ? 1 : 0
 
   alarm_name          = var.alarm_suffix == "" ? "vpc-natgw-active-cx-count-alarm" : "vpc-natgw-active-cx-count-alarm-${var.alarm_suffix}"
-  comparison_operator = "EqualToThreshold"
+  comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "1"
   datapoints_to_alarm = "1"
   metric_name         = "ActiveConnectionCount"
@@ -22,5 +22,4 @@ resource "aws_cloudwatch_metric_alarm" "alarm_cw_natgw_active_cx" {
   threshold           = "0"
   alarm_description   = "VPC Nat Gateway CW ActiveConnectionCount Rule has been triggered"
   alarm_actions       = [data.aws_sns_topic.main.arn]
-  ok_actions          = [data.aws_sns_topic.main.arn]
 }
